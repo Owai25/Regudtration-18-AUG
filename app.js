4cc40a5ac111277a358562d1cbaa31dcb50afed6
Owais Ateeq <owaisatiq.7@gmail.com>
2024-09-27 21:29:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,135 @@
+// alert("Owais");
+// var fullName = "Owais   Atiq";
+
+// alert(fullName)
+// alert (fullName)
+// alert (fullName)
+// alert (fullName)
+
+// var a = "owais";
+// a = "stop";
+// alert(a)                )))  BODMAS ==> bracket ord dvzn mult +,-  )))
+
+// var num1 = "10";        
+// var num2 = 7
+// var num3 = 4
+// alert ( num1+num2+num3 );
+
+// var name =  "Owais    atiq";
+// console.log(name)
+// var message = Owais + " " + Atiq;
+
+
+// var userAge = +prompt("Enter Your Age");
+
+// if(userAge >= 16 && userAge <=50 ){
+//     console.log("Allow")
+// }
+// else{
+//     console.log ("Not Allow")
+// }
+
+
+// var userAge = +prompt("Enter Your Age");
+
+// if (userAge >= 16 && userAge <=50 ){
+//     console.log("Allow")
+// }else{
+//     console.log("Not Allow")
+// }
+
+// var email = prompt("Enter Your Email")
+// var password = prompt("Enter your Password")
+
+// if(email === "Owais@gmail.com" && password === "Almout")
+// {
+//     console.log("login successfully")
+
+// }
+// else{
+//     console.log("Your password or gmail is Invalid")
+// }
+
+
+// var email = prompt("Enter Your Email")
+// var password = prompt("Enter Your password")
+
+// if(email === "owais@gmail.com" && password === "almout")
+//  {   console.log("Login successfully")}
+// else{
+//     console.log("Invalid password or email")
+// }
+//   if(email === "owais@gmail.com")
+
+//        {
+//             if(password ==="almout")
+//             {
+//                 console.log("successfully")
+//             }else{
+//                 console.log("invalid password")
+//             }
+//        }
+
+//   else{
+//     console.log("email is incorrect")
+//   }
+
+        // ARRAYS
+
+
+        // var cities = ["karachi","lahore","islamabad","multan"]
+
+        // console.log(cities[3], cities[1])
+
+        // var cities = ["lahore", "multan"]
+        // cities[5] = "karachi";
+        // console.log(cities ,"cities");
+
+
+// var cities = ["karachi","multan ", "islamabad","bareli"]
+// console.log(cities[0], cities[2])
+
+
+// var cities = ["karachi","lahore","bareli" ]
+
+// alert("welcome to" + " " + cities[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
